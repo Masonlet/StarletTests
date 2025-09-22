ac06a47bb0bb1bfb8352e968362bba642bc4d0e3
Masonlet <masonletoile@hotmail.com>
2025-09-22 13:50:41 -0400

    Initial build files

---
+++ tests/test_math.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "StarletMath/mat4.hpp"
+#include "StarletMath/transform.hpp"
+#include "StarletMath/vec2.hpp"
+#include "StarletMath/vec3.hpp"
+#include "StarletMath/vec4.hpp"
+#include "StarletMath/vertex.hpp"
